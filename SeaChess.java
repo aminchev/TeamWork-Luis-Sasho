87e30937878eb792e16626d404619f54d4a83d8c
Luis <luisgsaiz@hotmail.com>
2015-12-08 18:57:57 +0200

    Check bug fixed (char default)

--- SeaChess.java
+++ SeaChess.java
@@ -1,168 +1,160 @@
 import java.util.Scanner;
 
 public class SeaChess {
-	
-	public static int[] enterCoordinates(int row, int col){
+
+	public static int[] enterCoordinates(int row, int col) {
 		Scanner sc = new Scanner(System.in);
-		while(row<0 || row >2 || col<0 || col>2){
+		while (row < 0 || row > 2 || col < 0 || col > 2) {
 			System.out.println("Please enter correct coordinates for position of your simbol");
 			row = sc.nextInt();
 			col = sc.nextInt();
 		}
-		return new int[]{row,col};
+		return new int[] { row, col };
 	}
-	public static void	freePosition(char[][] board, int row, int col, int counter){
+
+	public static void freePosition(char[][] board, int row, int col, int counter) {
 		Scanner sc = new Scanner(System.in);
-		int[] array =  enterCoordinates(row, col);
+		int[] array = enterCoordinates(row, col);
 		row = array[0];
 		col = array[1];
-		while(board[row][col] == 'X' || board[row][col] == 'O'){
-				System.out.println("Position with coordinates " + row + ", " +col + " is not free. "
-						+ "Please enter coordinates of free position");
-				row = sc.nextInt();
-				col = sc.nextInt();
-				array = enterCoordinates(row, col);
-				row = array[0];
-				col = array[1];
-			}
-		
-		if(counter%2 == 1){
-			board[row][col] = 'X';
+		while (board[row][col] == 'X' || board[row][col] == 'O') {
+			System.out.println("Position with coordinates " + row + ", " + col + " is not free. "
+					+ "Please enter coordinates of free position");
+			row = sc.nextInt();
+			col = sc.nextInt();
+			array = enterCoordinates(row, col);
+			row = array[0];
+			col = array[1];
 		}
-		else{
+
+		if (counter % 2 == 1) {
+			board[row][col] = 'X';
+		} else {
 			board[row][col] = 'O';
 		}
 	}
 
 	public static void main(String[] args) {
-		char[][]board = new char[3][3];
+		char[][] board = new char[3][3];
 		boolean winner = false;
 		Scanner sc = new Scanner(System.in);
 		int counter = 1;
-		while(counter<=9){
-			if(counter%2 == 1){
+		while (counter <= 9) {
+			if (counter % 2 == 1) {
 				System.out.println("Player 1: Enter coordinates for you symbol:");
 				int row = sc.nextInt();
 				int col = sc.nextInt();
 				freePosition(board, row, col, counter);
 			}
-			if(counter%2 == 0){
+			if (counter % 2 == 0) {
 				System.out.println("Player 2: Enter coordinates for your symbol:");
 				int row = sc.nextInt();
 				int col = sc.nextInt();
 				freePosition(board, row, col, counter);
 			}
-			
+
 			for (int i = 0; i < board.length; i++) {
 				for (int j = 0; j < board.length; j++) {
 					System.out.print(board[i][j] + " ");
 				}
 				System.out.println();
 			}
-			if(counter>=5){
-				if(board[0][0]==board[0][1] && board[0][0]==board[0][2] && board[0][0]!=' '){
-					if(counter%2 == 1){
+			if (counter >= 1) {
+				if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[1][0]==board[1][1] && board[1][0]==board[1][2] && board[1][0]!=' ' ){
-					if(counter%2 == 1){
+				if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[2][0]==board[2][1] && board[2][0]==board[2][2] && board[2][0]!=' '){
-					if(counter%2 == 1){
+				if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[0][0]==board[1][0] && board[0][0]==board[2][0] && board[0][0]!=' '){
-					if(counter%2 == 1){
+				if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[0][1]==board[1][1] && board[0][1]==board[2][1] && board[0][1]!=' '){
-					if(counter%2 == 1){
+				if (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[0][2]==board[1][2] && board[0][2]==board[2][2] && board[0][2]!=' '){
-					if(counter%2 == 1){
+				if (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[0][0]==board[1][1] && board[0][0]==board[2][2]&& board[0][0]!=' ' ){
-					if(counter%2 == 1){
+				if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
-				if(board[0][2]==board[1][1] && board[0][2]==board[2][0] && board[0][2]!=' '){
-					if(counter%2 == 1){
+				if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != '\u0000') {
+					if (counter % 2 == 1) {
 						System.out.println("Player 1 win the game");
-						winner=true;e
+						winner = true;
 						break;
-					}
-					else{
+					} else {
 						System.out.println("Player 2 win the game");
-						winner=true;
+						winner = true;
 						break;
 					}
 				}
 			}
-			counter++;		
+			counter++;
 		}
-		if(winner == false){
+		if (winner == false) {
 			System.out.println("No winner! Try again!");
 		}
-			
+
 	}
-	}
+
 }
